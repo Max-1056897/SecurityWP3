9dbf6200d1c4a96293f4d277d04981eb8bc71850
Max-1056897 <1056897@hr.nl>
2023-02-15 11:06:34 +0100

    ADDED: Database en Flasklogin

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template, request, flash 
-import random
+import os.path
+import sqlite3
+import os.path
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, url_for, flash, request, redirect, send_file
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, ValidationError
+from lib.tablemodel import DatabaseModel
+import random
+
 
 
 # Flask Settings
@@ -11,19 +19,85 @@ FLASK_PORT = 81
 FLASK_DEBUG = True
 
 app = Flask(__name__)
+app.secret_key = 'Hogeschoolrotterdam'
 
-@app.route('/login', methods=['GET','POST'])
+DATABASE = os.path.join(app.root_path, 'Database', 'aanmeldingstool.db')
+dbm = DatabaseModel(DATABASE)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+conn = sqlite3.connect(DATABASE)
+curs = conn.cursor()
+
+# Flaskform Login
+class LoginForm(FlaskForm):
+ username = StringField('Student_Naam',validators=[DataRequired()])
+ password = PasswordField('Student_ID',validators=[DataRequired()])
+ remember = BooleanField('Remember Me')
+ submit = SubmitField('Login')
+ def validate_username(self, username):
+    conn = sqlite3.connect(DATABASE)
+    curs = conn.cursor()
+    curs.execute("SELECT Student_Naam FROM Student where Student_ID = (?)",[username.data])
+    valusername = curs.fetchone()
+    if valusername is None:
+      raise ValidationError('This username ID is not registered. Please register before login')
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+         self.id = id
+         self.username = username
+         self.password = password
+         self.authenticated = False
+    def is_active(self):
+         return self.is_active()
+    def is_anonymous(self):
+         return False
+    def is_authenticated(self):
+         return self.authenticated
+    def is_active(self):
+         return True
+    def get_id(self):
+        return self.id
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = sqlite3.connect(DATABASE)
+    curs = conn.cursor()
+    curs.execute("SELECT * from student where Klas_ID = (?)",[user_id])
+    lu = curs.fetchone()
+    if lu is None:
+      return None
+    else:
+      return User(int(lu[0]), lu[1], lu[2])
+
+@app.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+  if current_user.is_authenticated:
+    return redirect(url_for('random-code-generator'))
+  form = LoginForm()
+  if form.validate_on_submit():
+    conn = sqlite3.connect(DATABASE)
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM student where Student_Naam = (?)",    [form.username.data])
+    user = curs.fetchone()
+    Us = load_user(user[0])
+    if form.username.data == Us.username and form.password.data == Us.password:
+        login_user(Us)
+        return redirect(('random-code-generator'))
+    else:
+        flash('Login Unsuccessfull.')
+  return render_template('student-attendence.html',title='Login', form=form)
 
-@app.route('/', methods=['GET','POST'])
-def home():
-    return render_template("student-attendence.html")
+
+@app.route("/")
+def redirectpage():
+    return redirect("login")
 
 @app.route('/teacher-portal', methods=['GET','POST'])
 def techerPortal():
     return render_template("teacher-portal.html")
-
 
 def get_value():
     low = 100000
@@ -34,6 +108,7 @@ def get_value():
     return value
 
 @app.route('/random-code-generator', methods=['GET','POST'])
+@login_required
 def randomCodeGenerator():
     value_generator = ""
     if request.method == 'POST':
--- dbapp.py
+++ dbapp.py
@@ -1,49 +1,58 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 # create the extension
-db = SQLAlchemy()
+sqlite3.connect('aanmeldingstool.db')
 # create the app
 app = Flask(__name__)
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///aanmeldingstool.db"
-# initialize the app with the extension
-db.init_app(app)
+conn = sqlite3.connect('aanmeldingstool') 
+c = conn.cursor()
 
-class Klas(db.Model):
-    Klas_ID = db.Column(db.Integer, primary_key=True)
-    Klas_Naam = db.Column(db.String, unique=True, nullable=False)
+c.execute('''
+          CREATE TABLE IF NOT EXISTS student
+          ([Student_id] INTEGER PRIMARY KEY, [Student_Naam] TEXT, [Student_Nummer] INTEGER)
+          ''')
+                     
+conn.commit()
 
-class Student(db.Model):
-    Student_ID = db.Column(db.Integer, primary_key=True)
-    Student_Naam = db.Column(db.String, unique=True, nullable=False)
-    Klas_ID = db.Column(db.Integer, db.ForeignKey(Klas.Klas_ID))
 
-class Docent(db.Model):
-    Docent_ID = db.Column(db.Integer, primary_key=True)
-    Docent_Naam = db.Column(db.String, unique=True, nullable=False)
 
-class Les(db.Model):
-    Les_ID = db.Column(db.Integer, primary_key=True)
-    Start_Tijd = db.Column(db.Integer, unique=False, nullable=False)
-    Eind_Tijd = db.Column(db.Integer, unique=False, nullable=False)
-    Docent_ID = db.Column(db.Integer, db.ForeignKey(Docent.Docent_ID))
 
-class Aanwezigheid(db.Model):
-    Aanwezigheid_ID = db.Column(db.Integer, primary_key=True)
-    Tijdstip = db.Column(db.Integer, unique=False, nullable=False)
-    Les_ID = db.Column(db.Integer, db.ForeignKey(Les.Les_ID))
-    Student_ID = db.Column(db.Integer, db.ForeignKey(Student.Student_ID))
+# class Klas(db.Model):
+#     Klas_ID = db.Column(db.Integer, primary_key=True)
+#     Klas_Naam = db.Column(db.String, unique=True, nullable=False)
 
-class Vraag(db.Model):
-    Vraag_ID = db.Column(db.Integer, primary_key=True)
-    Vraag_Tekst = db.Column(db.String, unique=True, nullable=False)
+# class Student(db.Model):
+#     Student_ID = db.Column(db.Integer, primary_key=True)
+#     Student_Nummer = db.Column(db.Integer, unique=True, nullable=False)
+#     Student_Naam = db.Column(db.String, unique=True, nullable=False)
+#     Klas_ID = db.Column(db.Integer, db.ForeignKey(Klas.Klas_ID))
 
-class Antwoord(db.Model):
-    Antwoord_ID = db.Column(db.Integer, primary_key=True)
-    Antwoord_Tekst = db.Column(db.String, unique=True, nullable=False)
-    Vraag_ID = db.Column(db.Integer, db.ForeignKey(Vraag.Vraag_ID))
-    Aanwezigheid_ID = db.Column(db.Integer, db.ForeignKey(Aanwezigheid.Aanwezigheid_ID))
+# class Docent(db.Model):
+#     Docent_ID = db.Column(db.Integer, primary_key=True)
+#     Docent_Naam = db.Column(db.String, unique=True, nullable=False)
 
-with app.app_context():
-    db.create_all()
+# class Les(db.Model):
+#     Les_ID = db.Column(db.Integer, primary_key=True)
+#     Start_Tijd = db.Column(db.Integer, unique=False, nullable=False)
+#     Eind_Tijd = db.Column(db.Integer, unique=False, nullable=False)
+#     Docent_ID = db.Column(db.Integer, db.ForeignKey(Docent.Docent_ID))
+
+# class Aanwezigheid(db.Model):
+#     Aanwezigheid_ID = db.Column(db.Integer, primary_key=True)
+#     Tijdstip = db.Column(db.Integer, unique=False, nullable=False)
+#     Les_ID = db.Column(db.Integer, db.ForeignKey(Les.Les_ID))
+#     Student_ID = db.Column(db.Integer, db.ForeignKey(Student.Student_ID))
+
+# class Vraag(db.Model):
+#     Vraag_ID = db.Column(db.Integer, primary_key=True)
+#     Vraag_Tekst = db.Column(db.String, unique=True, nullable=False)
+
+# class Antwoord(db.Model):
+#     Antwoord_ID = db.Column(db.Integer, primary_key=True)
+#     Antwoord_Tekst = db.Column(db.String, unique=True, nullable=False)
+#     Vraag_ID = db.Column(db.Integer, db.ForeignKey(Vraag.Vraag_ID))
+#     Aanwezigheid_ID = db.Column(db.Integer, db.ForeignKey(Aanwezigheid.Aanwezigheid_ID))
+
+# with app.app_context():
+#     db.create_all()
